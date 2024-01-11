554f233d22f645b7b913b633398224e1bc8e4c0b
EhsanRanjoory <154647666+EhsanRanjoory@users.noreply.github.com>
2024-01-12 01:20:56 +0330

    Add files via upload

---
+++ script/1001allCenterLogo.js
@@ -0,0 +1,30 @@
+﻿var Site={"List":
+[
+{"FaName":"هزارویک کتاب","EnName":"1001ketab.ir","URL":"http://1001ketab.ir/home"}
+,
+{"FaName":"شرکت هزارویک کتاب","EnName":"1001ketab.co.ir","URL":"http://1001ketab.co.ir"}
+,
+{"FaName":"هزارویک سوفا","EnName":"1001sofa.ir","URL":"http://1001sofa.ir"}
+]
+};
+const Lower=0;
+const Upper=3;  //Site.List[].lenth;
+Index = Lower;
+function Plus(){
+	Index++;
+	if(Index >= Upper)
+		Index = Lower;
+	CenterLogoObj = document.getElementById("centerlogo");
+	CenterLogoObj.href = Site.List[Index].URL;
+	CenterLogoObj.innerHTML = Site.List[Index].EnName + "<br />" + Site.List[Index].FaName;
+	return;
+}
+function Minos(){
+	Index--;
+	if(Index < Lower)
+		Index = Upper-1;
+	CenterLogoObj = document.getElementById("centerlogo");
+	CenterLogoObj.href = Site.List[Index].URL;
+	CenterLogoObj.innerHTML = Site.List[Index].EnName + "<br />" + Site.List[Index].FaName;
+	return;
+}
---
+++ script/1001allChangeBG.js
@@ -0,0 +1,7 @@
+﻿function ChangeBG(BGFileNum){
+	var LoadImage = new Image();
+	LoadImage.src = "style/b" + BGFileNum + ".jpg";
+	C1Obj = document.getElementById("c1");
+	C1Obj.style.backgroundImage = "url(style/b" + BGFileNum + ".jpg)";
+	return;
+}
