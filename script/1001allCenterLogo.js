9cd9e1762425e33cbdad97b92c748b88ffe46143
EhsanRanjoory <154647666+EhsanRanjoory@users.noreply.github.com>
2024-01-15 06:45:32 +0330

    Add files via upload

--- script/1001allCenterLogo.js
+++ script/1001allCenterLogo.js
@@ -1,14 +1,16 @@
 ﻿var Site={"List":
 [
-{"FaName":"هزارویک کتاب","EnName":"1001ketab.ir","URL":"http://1001ketab.ir/home"}
+{"FaName":"هزارویک کتاب","EnName":"1001ketab.ir","URL":"http://1001ketab.ir/"}
 ,
 {"FaName":"شرکت هزارویک کتاب","EnName":"1001ketab.co.ir","URL":"http://1001ketab.co.ir"}
+,
+{"FaName":"هزارویک سرچ","EnName":"1001search.ir","URL":"http://1001search.ir"}
 ,
 {"FaName":"هزارویک سوفا","EnName":"1001sofa.ir","URL":"http://1001sofa.ir"}
 ]
 };
 const Lower=0;
-const Upper=3;  //Site.List[].lenth;
+const Upper=4;  //Site.List[].length;
 Index = Lower;
 function Plus(){
 	Index++;
